528ed95f1781ac8d1b6a06f234b85b22eed64de0
Daren Drahoš <45952291+Dazza9905@users.noreply.github.com>
2024-11-05 23:39:25 +0100

    belt added, just art tho

---
+++ testing/testing.gd
@@ -0,0 +1,12 @@
+extends Label
+
+# Called when the node enters the scene tree
+func _ready():
+	pass
+
+# Called every frame
+func _process(delta):
+	# Get the current FPS
+	var fps = Engine.get_frames_per_second()
+	# Update the label text
+	self.text = "FPS: " + str(fps)
