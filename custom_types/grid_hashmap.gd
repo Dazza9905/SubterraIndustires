091a082fbece875e47053aaab335276b2b3eb8ce
Daren Drahoš <45952291+Dazza9905@users.noreply.github.com>
2024-11-18 00:58:27 +0100

    grid objects are propperly added and removed from hash map

--- custom_types/grid_hashmap.gd
+++ custom_types/grid_hashmap.gd
@@ -1,9 +1,42 @@
 extends Node	
 
+
+
 var grid_hashmap: Dictionary = {"bannana": 1}
 var x: int = 3
+	
+func _on_build_succesfull(grid_object: GridObject):
+	var position: Vector2i
+	position.x = (grid_object.position.x - 16) / 32
+	position.y = (grid_object.position.y - 16) / 32
+	
+	#get indicators (for teir position)
+	var collision_indicators = $"../../World/GridPositioner/ManipulationParent/RuleCheckIndicatorManager".get_children()
+	
+	#for each indicator create a entry in dictionary
+	for indicator in collision_indicators:
+		#calcualte the coordinate for occupied cell
+		var p_offset: Vector2i
+		p_offset.x = indicator.position.x / 32
+		p_offset.y = indicator.position.y / 32
+		var coordinate: Vector2i = position + p_offset
+		#add it to the object
+		grid_object.ocuppied_cells.append(coordinate)
+		
+	for cell in grid_object.ocuppied_cells:
+		grid_hashmap[cell] = grid_object
+		
+	print_dict()
+	
+	
+func _on_object_removed_from_grid(ocuppied_cells: Array[Vector2i]):
+	print(ocuppied_cells)
+	for cell in ocuppied_cells:
+		grid_hashmap.erase(cell)
+	print_dict()
 
-
-func _on_object_added_to_grid():
-	grid_hashmap["asda"] = 3
-	print(grid_hashmap["asda"])
+func print_dict():
+	for key in grid_hashmap.keys():
+		var value = grid_hashmap[key]
+		print("Key: ", key, " - Value: ", value)
+	print("-------------")
--- custom_types/grid_object.gd
+++ custom_types/grid_object.gd
@@ -2,18 +2,22 @@
 class_name GridObject
 extends StaticBody2D
 
-var coordinates: Vector2
-@export var ocuppied_cells: Array[Vector2]
+var coordinates: Vector2i
+@export var ocuppied_cells: Array[Vector2i]
 
-signal _object_added_to_grid
+#signal _object_added_to_grid
+signal _on_object_removed_from_grid
 
 func _connect_to_reference_system():
 	# Get the ReferenceSystem node using an absolute path
 	var reference_system = get_node("/root/Game/Systems/ReferenceSystem")
 	if reference_system:
-		var callable = Callable(reference_system, "_on_object_added_to_grid")
+		var call_add = Callable(reference_system, "_on_object_added_to_grid")
+		var call_remove = Callable(reference_system, "_on_object_removed_from_grid")
+		# FIXME: remove function from building_system.gd and use call_add
 		# Connect the signal to the _on_object_ready function in ReferenceSystem
-		connect("_object_added_to_grid", callable)
+		#connect("_object_added_to_grid", call_add)
+		connect("_on_object_removed_from_grid", call_remove)
 	else:
 		print("ReferenceSystem node not found!")
 
@@ -21,7 +25,10 @@ func _connect_to_reference_system():
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	_connect_to_reference_system()
-	_object_added_to_grid.emit()
+	
+func _exit_tree() -> void:
+	_connect_to_reference_system()
+	_on_object_removed_from_grid.emit(self.ocuppied_cells)
 	
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
