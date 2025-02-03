312f722ba6f1442703e8088a19b2b410f671dc8b
Daren Drahos <daren.drahos@gmail.com>
2025-02-03 01:10:39 +0100

    save

--- hud/objective/objective_ui.gd
+++ hud/objective/objective_ui.gd
@@ -1,19 +1,32 @@
-extends VBoxContainer
+extends Control
 class_name ObjectiveUI
+
+
+@export var objective_label: Label
+@export var sub_objectives_parent: Control
+@export var objective_menu: Control
 
 
 func _ready() -> void:
 	Globals.get_ObjectiveSystem().objective_changed.connect(_on_objective_changed)
-	$Label.text = Globals.get_ObjectiveSystem().current_objective.name
+	objective_label.text = Globals.get_ObjectiveSystem().current_objective.name
 	for sub_objective in Globals.get_ObjectiveSystem().current_objective.sub_objectives:
-		$VBoxContainer.add_child(SubObjectiveUI.new(sub_objective))
+		sub_objectives_parent.add_child(SubObjectiveUI.new(sub_objective))
 
 
 func _on_objective_changed() -> void:
-	for child in $VBoxContainer.get_children():
+	for child in sub_objectives_parent.get_children():
 		child.queue_free()
 	if Globals.get_ObjectiveSystem().current_objective:
-		$Label.text = Globals.get_ObjectiveSystem().current_objective.name
+		objective_label.text = Globals.get_ObjectiveSystem().current_objective.name
 		for sub_objective in Globals.get_ObjectiveSystem().current_objective.sub_objectives:
-			$VBoxContainer.add_child(SubObjectiveUI.new(sub_objective))
+			sub_objectives_parent.add_child(SubObjectiveUI.new(sub_objective))
+	
+
+
+func _on_open_objectives_menu_button_pressed() -> void:
+	if objective_menu.visible:
+		objective_menu.visible = false
+	else: 
+		objective_menu.visible = true
 	
--- hud/objective/sub_objective_ui.gd
+++ hud/objective/sub_objective_ui.gd
@@ -6,5 +6,12 @@ class_name SubObjectiveUI
 func _init(pass_sub_objective: SubObjective) -> void:
 	sub_objective = pass_sub_objective
 
+func _ready() -> void:
+	var l_settings: LabelSettings = LabelSettings.new()
+	l_settings.font = preload("res://hud/pixel_font.ttf")
+	l_settings.font_size = 20
+	
+	label_settings = l_settings
+
 func _process(delta: float) -> void:
 	text = sub_objective.name + " " + str(sub_objective.current_completion) + "/" + str(sub_objective.finish_completion)
